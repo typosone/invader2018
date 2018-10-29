485f95c3d378cc9b1c484646fb3072fe4afc48bc
Nakasone Tsuyoshi <tsuyoshi.nakasone@gmail.com>
2018-10-29 12:24:01 +0900

    メインシーンの基礎が完成

--- js/invader.js
+++ js/invader.js
@@ -4,6 +4,20 @@ phina.globalize();
 const SCREEN_WIDTH = 960;
 const SCREEN_HEIGHT = 640;
 
+phina.define('MainScene', {
+    superClass: 'DisplayScene',
+    init: function () {
+        this.superInit({
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
+        });
+        this.gridX = Grid(SCREEN_WIDTH, 40);
+        this.gridY = Grid(SCREEN_HEIGHT, 40);
+
+        this.backgroundColor = 'black';
+    }
+});
+
 phina.main(() => {
     const app = GameApp({
         title: "インベーダー",
