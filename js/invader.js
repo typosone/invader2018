deb238f6e0573a50a19f337515f8690616079663
Nakasone Tsuyoshi <tsuyoshi.nakasone@gmail.com>
2018-11-05 10:37:07 +0900

    アセットの定義とプレイヤースプライトの表示

--- js/invader.js
+++ js/invader.js
@@ -3,6 +3,16 @@ phina.globalize();
 
 const SCREEN_WIDTH = 960;
 const SCREEN_HEIGHT = 640;
+const ASSETS = {
+    "image": {
+        "buro": "./assets/image/buropiyo.png",
+        "mero": "./assets/image/meropiyo.png",
+        "mika": "./assets/image/mikapiyo.png",
+        "nasu": "./assets/image/nasupiyo.png",
+        "take": "./assets/image/takepiyo.png",
+        "toma": "./assets/image/tomapiyo.png"
+    }
+};
 
 phina.define('MainScene', {
     superClass: 'DisplayScene',
@@ -15,6 +25,12 @@ phina.define('MainScene', {
         this.gridY = Grid(SCREEN_HEIGHT, 40);
 
         this.backgroundColor = 'black';
+
+        const player = Sprite('toma', 64, 64).addChildTo(this);
+        player.setFrameIndex(10, 64, 64);
+        player.x = this.gridX.center();
+        player.y = this.gridY.span(37);
+
     }
 });
 
@@ -24,6 +40,7 @@ phina.main(() => {
         fps: 60,
         width: SCREEN_WIDTH,
         height: SCREEN_HEIGHT,
+        assets: ASSETS,
     });
 
     app.run();
